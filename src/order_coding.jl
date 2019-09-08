23f8e0cf28e0eb225a937e79add2a4c59f041991
Michael Mattocks <mike.mattocks@utoronto.ca>
2019-09-08 00:42:55 -0400

    removed extraneous order_coding progressmeters

--- src/order_coding.jl
+++ src/order_coding.jl
@@ -44,7 +44,7 @@ function get_order_n_seqs(seqs::Vector{DNASequence}, order_no::Int64, base_tuple
     length_vec = Vector{Int64}()
     window = order_no + 1
 
-    @showprogress 1 "Getting order kmers..." for seq in seqs
+    for seq in seqs
         kmer_vec = Vector{Kmer}()
         @inbounds for (i, kmer) in collect(each(Kmer{DNA,window},seq))
             push!(kmer_vec, kmer)
@@ -61,7 +61,7 @@ end
 function code_seqs(input::N_Order_ntSequence, offsets::Array{Int64}=[0 for i in 1:length(input.order_kmers)])
     alphabet = input.alphabet
     output = zeros(Int64, (maximum([length(seq) for seq in input.order_kmers])+1), length(input.order_kmers)) #leave 1 missing value after the longest sequence for indexing sequence length in MS_HMMBase messages
-    @showprogress 1 "Coding sequences..." for (i, seq) in enumerate(input.order_kmers)
+    for (i, seq) in enumerate(input.order_kmers)
         for t in 1:length(seq)
             curr_kmer = input.order_kmers[i][t]
             curr_code = alphabet.symbols[curr_kmer]
